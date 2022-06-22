1d5da40543cabd51825967cdb88b122fab7ad2fa
ArssGoetia <lalon742@gmail.com>
2022-06-22 04:15:11 -0500

    Main page almost complete

--- script.js
+++ script.js
@@ -8,12 +8,19 @@ function loadprojects(){
             data.forEach(function(pages){
                 html += `
                 <div class="card">
-                    <img src="Assets/img/1.PNG${pages.image}" alt="">
+                    <img src="${pages.image}" alt="">
                     <p class="card_title">${pages.title}</p>
                     <p class="card_info">${pages.description}</p>
                     <div class="card_buttons">
-                        <a href="Projects/Day ${pages.page}/index.html"><button id="preview">Preview</button></a>
-                        <a href="${pages.link}"><button id="code">Code</button>
+                        <a href="Projects/Day ${pages.page}/index.html"><svg xmlns="http://www.w3.org/2000/svg"  id="preview" class="icon icon-tabler icon-tabler-eye" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <circle cx="12" cy="12" r="2" />
+                        <path d="M22 12c-2.667 4.667 -6 7 -10 7s-7.333 -2.333 -10 -7c2.667 -4.667 6 -7 10 -7s7.333 2.333 10 7" />
+                      </svg></a>
+                        <a href="${pages.link}"><svg xmlns="http://www.w3.org/2000/svg" id="code" class="icon icon-tabler icon-tabler-brand-github" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
+                      </svg></a>
                     </div>
                 </div>
                 `;
